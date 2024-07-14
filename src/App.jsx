d1617e037c2e1d0b0a5117170413f5fefa43e447
Rachaelj99 <rachaeljeon99@gmail.com>
2024-07-14 15:11:56 -0400

    clean up App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,11 +15,10 @@ import {Contact} from './client/Contact'
 import {MainPage} from './client/MainPage'
 import {Interests} from './client/Interests'
 
-let url = 'http://localhost:5000' // backend is running on 8000
+export default function App() {
 
-function App() {
+  const [todos, setTodos] = useState([])
 
-  const [todos, setTodos] = useState([]);
   useEffect(() => {
     fetch("/todos").then(response => 
     response.json().then(data =>
@@ -29,49 +28,46 @@ function App() {
 
   return (
     <>
-    <p></p>
-    <Router>
-    <Navbar bg="light" expand="lg" sticky="top">
-      <Container>
-        <Navbar.Brand href="/">Rachael Jeon's Blog</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-          <Nav>
-            {/* <Nav.Link href="/home">Home</Nav.Link> */}
-            <Nav.Link href="/resume">Resume</Nav.Link>
-            <Nav.Link href="/projects">Projects</Nav.Link>
-            <Nav.Link href="https://github.com/rachaeljeon">Github</Nav.Link>
-            <Nav.Link href="/interests">Interests</Nav.Link>
-            <NavDropdown title="More" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/aboutme">About Me</NavDropdown.Item>
-              <NavDropdown.Item href="/contact">Contact</NavDropdown.Item>
-              <NavDropdown.Divider />
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-      <Routes>
-        <Route path="/" element={MainPage()}>
-        </Route>
-        {/* <Route path="/home" element={HomePage()}>
-        </Route> */}
-        <Route path="/resume" element={Resume()}>
-        </Route>
-        <Route path="/projects" element={todos}>
-        </Route>
-        <Route path="/github" element={Github()}>
-        </Route>
-        <Route path="/interests" element={Interests()}>
-        </Route>
-        <Route path="/aboutme" element={AboutMe()}>
-        </Route>
-        <Route path="/contact" element={<Contact url={url}/>}>
-        </Route>
-      </Routes>
-    </Router>
+      <Router>
+        <Navbar bg="light" expand="lg" sticky="top">
+          <Container>
+            <Navbar.Brand href="/">Rachael Jeon's Blog</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#resume">Resume</Nav.Link>
+                <Nav.Link href="#projects">Projects</Nav.Link>
+                <Nav.Link href="https://github.com/rachaeljeon">Github</Nav.Link>
+                <Nav.Link href="#interests">Interests</Nav.Link>
+                <NavDropdown title="More" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#aboutme">About Me</NavDropdown.Item>
+                  <NavDropdown.Item href="#contact">Contact</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Routes>
+          <Route path="/" element={MainPage()}>
+          </Route>
+          {/* <Route path="/home" element={HomePage()}>
+          </Route> */}
+          <Route path="/resume" element={Resume()}>
+          </Route>
+          <Route path="/projects" element={todos}>
+          </Route>
+          <Route path="/github" element={Github()}>
+          </Route>
+          <Route path="/interests" element={Interests()}>
+          </Route>
+          <Route path="/aboutme" element={AboutMe()}>
+          </Route>
+          <Route path="/contact" element={<Contact url={url}/>}>
+          </Route>
+        </Routes>
+      </Router>
     </>
   );
 }
-
-export default App;
