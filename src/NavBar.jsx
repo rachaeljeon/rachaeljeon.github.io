17c8ec73f245e7e17abfefb23cdeb0752690f9df
Rachaelj99 <rachaeljeon99@gmail.com>
2024-07-14 16:22:04 -0400

    moved the pages on navbar to the far right

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -4,12 +4,12 @@ import {Container, Nav,  Navbar, NavDropdown} from 'react-bootstrap';
 
 export default function NavBar() {
     return (
-        <Navbar bg="light" expand="lg" sticky="top">
+        <Navbar expand="lg" className="bg-body-tertiary">
           <Container>
-            <Navbar.Brand href="/">Rachael Jeon's Blog</Navbar.Brand>
+            <Navbar.Brand as={Link} to="/">Rachael Jeon's Blog</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-              <Nav className="me-auto">
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
                 <Nav.Link as={Link} to="/home">Home</Nav.Link>
                 <Nav.Link as={Link} to="/resume">Resume</Nav.Link>
                 <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
